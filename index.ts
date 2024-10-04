aba34813f63d67c6e2406fbd2d0756d1f94eda25
ScuBix1 <bastian.monnin1@gmail.com>
2024-10-04 10:14:21 +0200

    creation de la game

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 const app: Express = express()
-const port = 5000
+const port: number = 5000
 import path from 'path'
 import { createGame, getGames } from "./controllers/player.controller"
 
 const front = path.normalize(path.join(__dirname, '..','..','Puissance-4_Puissance_a_4'))
 console.log(`Serve ${front}`)
 
+app.use(cors());
 app.use(express.json())
 app.use('/', express.static(front))
-app.get('/games', getGames)
-app.post('/games', createGame)
+app.get('/rooms', getGames)
+app.post('/rooms', createGame)
 
 app.listen(port, ()=>{
     console.log(`Serveur démarré sur le port ${port}`)
--- models/game.ts
+++ models/game.ts
@@ -1,13 +1,11 @@
 import { Player } from './player';
 
 export class Game {
-  player1: Player;
-  player2: Player;
+  players: Player[];
   name: string;
   id: number;
-  constructor(player1: Player, player2: Player, name: string, id: number) {
-    this.player1 = player1;
-    this.player2 = player2;
+  constructor(players: Player[], name: string, id: number) {
+    this.players = players;
     this.name = name;
     this.id = id;
   }
