b519a77dc5a604461eadf6b512009ef4e0f899b3
Shannon Besson <shannon0413@outlook.com>
2024-10-04 15:37:07 +0200

    methode post avec seulement 1 resultat

--- models/room.ts
+++ models/room.ts
@@ -2,7 +2,7 @@ import { Game } from './game';
 import { Player } from './player';
 
 export class Room {
-    readonly id!: number;
+    id!: number;
     name!: string;
     players: Player[] = [];
 }
